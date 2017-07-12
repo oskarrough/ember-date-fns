ab21fc930667f1390db143d6d9291dfa85988bdb
Laurin Quast <laurinquast@googlemail.com>
2017-07-12 09:48:20 +0200

    Adds support for exposing additional date-fns via `ember-cli-build.js`

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin');
 
 module.exports = {
   name: 'ember-date-fns',
-  included() {
+  included(app) {
     this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
     this.import('vendor/-ember-date-fns-bundle.js');
     this.import('vendor/-ember-date-fns-shims.js');
+
+    const config = app.options[this.name] || {};
+    this.includedDateFns = config.includedDateFns || [];
   },
   treeForVendor() {
+    const exposedFunctions = [
+      'format',
+      'distance_in_words_to_now'
+    ].concat(this.includedDateFns);
+
     return new WebpackDependencyPlugin({
       outputName: 'ember-date-fns',
-      expose: [
-        'date-fns/format',
-        'date-fns/distance_in_words_to_now',
-      ]
+      expose: exposedFunctions.map(name => `date-fns/${name}`)
     });
   }
 };
