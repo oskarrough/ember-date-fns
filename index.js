050211550f40a3f5752646f4b8093d416f6d288a
Bujorel Tecu <btecu@outlook.com>
2019-01-24 15:37:17 -0500

    Update `ember-cli` and dependencies. Use `ember-auto-import` instead of custom `webpack`. Closes #14.

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 'use strict';
 
-const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin');
-
 module.exports = {
-  name: 'ember-date-fns',
+  name: require('./package').name,
 
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    this.import('vendor/-ember-date-fns-bundle.js');
-    this.import('vendor/-ember-date-fns-shims.js');
-
-    let config = app.options[this.name] || {};
-    this.includedDateFns = config.includedDateFns || [];
-  },
-
-  treeForVendor() {
-    let exposedFunctions = [
-      'format',
-      'distance_in_words_to_now'
-    ]
-    .concat(this.includedDateFns)
-    .map(name => `date-fns/${name}`);
-
-    return new WebpackDependencyPlugin({
-      outputName: 'ember-date-fns',
-      expose: [...new Set(exposedFunctions)]
-    });
+  options: {
+    autoImport: {
+      exclude: [],
+      webpack: { }
+    }
   }
 };
